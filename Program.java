36f0436c5f9089f51f714a3215516442a2250e11
SidDidntDoAnything <77165813+SidDidntDoAnything@users.noreply.github.com>
2024-08-28 12:11:02 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,33 @@
 //import stuff here
 
 //Your code here
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double n1;
+        double n2;
+        System.out.print("\nEnter first number: ");
+        n1 = sc.nextDouble();
+        System.out.print("\nEnter second number: ");
+        n2 = sc.nextDouble();
+        double sum = n1 + n2;
+        double difference = n1 - n2;
+        double product = n1 * n2;
+        double avg = (n1 + n2)/2;
+        if (n1 > n2) {
+            double absval = n1 - n2;
+        }
+        else {
+            double absval = n2 - n1;
+        }
+        
+        
 
+        }
+}
 //Paste console output below:
 /*
 
