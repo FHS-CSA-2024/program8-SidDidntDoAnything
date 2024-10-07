1487ab3f46e6863f2aef41acf63d83349189074a
Benjamin Johnson <bjohnson@fcusd.org>
2024-10-07 11:37:28 -0700

    Error in code

--- Program.java
+++ Program.java
@@ -4,6 +4,8 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
+
+//Class name does not match the file name
 class HelloWorld {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
